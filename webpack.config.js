e06f02c1943a1e3ef85f7930f696f12537033579
VerxMaxum <138268061+VerxMaxum@users.noreply.github.com>
2024-06-12 18:08:46 +0800

    Style initially

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css%/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin ({
+            filenmae: 'index.html',
+            template: 'src/index.html',
+            title: 'Weather',
+        }),
+    ],
+};
